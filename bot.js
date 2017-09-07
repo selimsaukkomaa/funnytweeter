a90a43b6093dc6bc4cc5d3ce0bb3065be8e71cab
Selim Saukkomaa <selim.saukkomaa@gmail.com>
2017-09-07 20:24:13 +0300

    add randomTweet posting function

--- bot.js
+++ bot.js
@@ -5,3 +5,31 @@ var Twit = require('twit');
 var config = require('./config');
 
 var T = new Twit(config);
+
+
+
+var params = {
+  screen_name: 'oskarionninen',
+  count: 10,
+}
+
+T.get('statuses/user_timeline', params, randomTweet);
+
+// Creating the random tweet from the group of tweets requested from the API.
+
+function randomTweet(err, data, response) {
+
+  var tweets = data;
+  var funnytweet = tweets[Math.floor(Math.random()*tweets.length)].text + " 😂"
+
+// Code for creating a test tweet.
+
+  var testtweet = { status: "Testing " + "😂" }
+
+// Code for posting to Twitter. Replace testtweet with funnytweet and the program will use a randomized tweet from the desired user's timeline.
+
+  T.post('statuses/update', testtweet, function(err, data, response) {
+    console.log(data)
+  })
+
+}
